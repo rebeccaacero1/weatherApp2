c71b35a5a2af38b6f31b8f784623d5e768f02d9f
rebeccaacero1 <rebeccaacero1@gmail.com>
2022-10-28 15:44:43 +0800

    addConstDisplayWeather

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 //https://api.openweathermap.org/data/2.5/weather?q=manila&appid=c62f1d779dacae7486cddeeadac8a080&units=metric
 
-let weather = {
+
+//Promise
+/* let weather = {
     apiKey: 'c62f1d779dacae7486cddeeadac8a080',
     fetchWeather : function(){
         fetch("https://api.openweathermap.org/data/2.5/weather?q=manila&appid=c62f1d779dacae7486cddeeadac8a080&units=metric").then
         ((Response) => Response.json()).then((data) => console.log(data));
+    }
+} */
+
+
+let weather = {
+    apiKey: 'c62f1d779dacae7486cddeeadac8a080',
+    fetchWeather : function(city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid="+ this.apiKey)
+        .then((Response) => Response.json())
+        .then((data) => this.displayweather(data));
+    },
+    displayweather: function(){
+        const{name} = data;
+        const{icon,description} = data.weather[0];
+        const{temp, humidity} = data.main;
+        const{speed} = data.wind;
+        console.log(name);
     }
 }
