18def676185a8ca0ac6005a7478c26bbcb0bc1da
rebeccaacero1 <rebeccaacero1@gmail.com>
2022-11-07 20:18:15 +0800

    balik loob

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@
 
 
 let weather = {
-    apiKey: 'c62f1d779dacae7486cddeeadac8a080',
+    apiKey: 'f86aa67b2119b865446c94ac3bf2ae3b',
     fetchWeather : function(city){
         fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid="+ this.apiKey)
         .then((Response) => Response.json())
@@ -25,5 +25,31 @@ let weather = {
         const{speed} = data.wind;
         console.log(name, icon, description, temp, humidity, speed);
         document.querySelector('.city').innerText="weather in " + name;
-    }
-}
+        document.querySelector('.icon').src="https://openweathermap.org/img/wn/"+ icon + "@2x.png"; 
+        document.querySelector('.description').innerText=description;
+        document.querySelector('.temp').innerText = temp + "°C";
+        document.querySelector('.humidity').innerText = "humidity:" + humidity + '%'
+        document.querySelector('.wind').innerText="wind: " + speed + "km/h"; 
+        document.querySelector('.weather').classList.remove("loading")
+        document.body.style.backgroundImage='url (https://source.unsplash.com/1600x900/?landscape)'
+        ;
+    },
+        
+
+    search: function(){
+        this.fetchWeather(document.querySelector('.search__bar').value)
+        }
+    };
+
+    document.querySelector('.search button'). addEventListener('click', function(){
+        weather.search();
+    })
+
+    document.querySelector('.search__bar').addEventListener('keyup', function(event){
+        if (event.key == "Enter"){
+            weather.search();
+        }
+                
+    })
+
+    weather.fetchWeather("Malabon");
